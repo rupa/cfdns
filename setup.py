642669f8b011304904345f4cfa63f15952bc2d63
rupa <rupa@lrrr.us>
2015-06-13 05:02:47 -0400

    make we a module

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='cfdns',
+    version='0.1',
+    description='dynamic DNS with cloudflare',
+    url='http://github.com/rupa/cfdns',
+    author='rupa',
+    author_email='rupa@lrrr.us',
+    license='MIT',
+    packages=['cfdns'],
+    entry_points={
+        'console_scripts': ['cfdns=cfdns:main'],
+    },
+    zip_safe=False
+)
